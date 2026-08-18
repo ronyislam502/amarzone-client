f48fd99036303e12936f3af13fefc19d3a45ab77
ronyislam502 <ronyislam502@gmail.com>
2026-08-18 23:46:35 +0600

    navbar

---
+++ src/app/(common)/(home)/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Navbar from '@/src/components/shared/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h2>This is home page</h2>
+        </div>
+    );
+};
+
+export default Home;
